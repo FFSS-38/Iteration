de3bfe6120054086275ce26f894e543ea5b44be5
Vincent Jacquemin <vincent@impulsenow.be>
2023-02-04 18:32:42 +0100

    database.js updated and MongoDB created

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,30 +1,22 @@
 const express = require('express');
-
-const starWarsController = require('../controllers/starWarsController');
-
+const Users = require('../database.js');
+const starWarsController = require('../controllers/Controller');
+const mongoose = require('mongoose');
 const router = express.Router();
 
-router.get('/', starWarsController.getCharacters, (req, res) =>
-  res.status(200).json(res.locals.ads)
-);
+router.get('/testroute', (req, res) => {
+  console.log('hello from lol');
+  Users.find({})
+    .then((userlist) => {
+      // console.log("hi from first chain")
+      res.send(userlist), console.log('hello from 9999');
+    })
+    .catch((err) => {
+      next({
+        log: 'ERROR ERROR',
+        message: { err: err },
+      });
+    });
+});
 
-router.get('/species', starWarsController.getSpecies, (req, res) =>
-  res.status(200).json(res.locals.species[0])
-);
-
-router.get('/homeworld', starWarsController.getHomeworld, (req, res) =>
-  res.status(200).json(res.locals.homeworld)
-);
-
-router.get('/film', starWarsController.getFilm, (req, res) =>
-  res.status(200).json({})
-);
-
-router.post('/createad', starWarsController.addCharacter, (req, res) =>
-  res.status(200).json({})
-);
-
-router.delete('/delete/:id', starWarsController.deleteAds, (req, res) =>
-  res.status(200).json(res.locals.deletems)
-);
 module.exports = router;
