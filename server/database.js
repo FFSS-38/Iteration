f03971ae4f0a13eb729ad664ccede4ba54ed5c06
Vincent Jacquemin <vincent@impulsenow.be>
2023-02-04 20:59:37 +0100

    finally connected the database and created a create and a find route

--- server/database.js
+++ server/database.js
@@ -10,26 +10,42 @@ const client = new MongoClient(
     serverApi: ServerApiVersion.v1,
   }
 );
-async function run() {
-  try {
-    await client.connect();
-    const database = client.db('Wonderpets');
-    const collection = database.collection('Users');
-    const docCount = await collection.countDocuments({});
-    console.log('doc count' + docCount);
-    // perform actions using client
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+mongoose
+  .connect(
+    'mongodb+srv://cluster0.g3hjzks.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority',
+    {
+      sslKey: credentials,
+      sslCert: credentials,
+      serverApi: ServerApiVersion.v1,
+    }
+    // {
+    //   // options for the connect method to parse the URI
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   // sets the name of the DB that our collections are part of
+    //   dbName: 'Wonderpets',
+    // }
+  )
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch((err) => console.log(err));
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  Name: { type: String, required: true },
+  Name: { type: String, required: true, unique: true },
+  Password: { type: String, required: true },
 });
+
+// async function run() {
+//   // try {
+//   await client.connect();
+//   const database = client.db('Wonderpets');
+//   const collection = database.collection('Users');
+//   const docCount = await collection.countDocuments({});
+
+// }
+// run().catch(console.dir);
 
 const petSchema = new Schema({
   Name: { type: String, required: true },
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,20 +1,71 @@
 const express = require('express');
-const Users = require('../database.js');
+const { Users, Pets } = require('../database.js');
 const starWarsController = require('../controllers/Controller');
 const mongoose = require('mongoose');
 const router = express.Router();
 
-router.get('/testroute', (req, res) => {
-  console.log('hello from lol');
-  Users.find({})
+// router.get('/testroute', (req, res) => {
+//   console.log('hello from lol');
+//   Users.find({})
+//     .then((userlist) => {
+//       console.log('hello from 9999');
+//       return res.status(200).json(userlist);
+//     })
+//     .catch((err) => {
+//       console.log('ERROR ERROR', err);
+//       return res.status(500).json({
+//         message: 'An error occurred while processing the request',
+//         error: err
+//       });
+//     });
+// });
+
+// router.get('/testroute', (req, res) => {
+//   console.log('hello from lol');
+//   Users.find({})
+//     .then((userlist) => {
+//       // console.log("hi from first chain")
+//       console.log('hello from 9999');
+//       return res.status(200).json(userlist);
+//     })
+//     .catch((err) => {
+//       next({
+//         log: 'ERROR ERROR',
+//         message: { err: err },
+//       });
+//     });
+// });
+
+router.post('/testroute', (req, res) => {
+  Users.create({ Name: 'CREATE2', Password: '1234' })
     .then((userlist) => {
-      // console.log("hi from first chain")
-      res.send(userlist), console.log('hello from 9999');
+      console.log('hello from 9999');
+      console.log(userlist);
+      return res.status(200).json(userlist);
     })
     .catch((err) => {
-      next({
-        log: 'ERROR ERROR',
-        message: { err: err },
+      console.log('ERROR ERROR', err);
+      return res.status(500).json({
+        message: 'An error occurred while processing the request',
+        error: err,
+      });
+    });
+});
+
+router.get('/testroute', (req, res) => {
+  console.log('hello from lol');
+  console.log(Users);
+  Users.find({})
+    .then((userlist) => {
+      console.log('hello from 9999');
+      console.log(userlist);
+      return res.status(200).json(userlist);
+    })
+    .catch((err) => {
+      console.log('ERROR ERROR', err);
+      return res.status(500).json({
+        message: 'An error occurred while processing the request',
+        error: err,
       });
     });
 });
